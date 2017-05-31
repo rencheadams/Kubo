60b09e478579857012f99a95bc97a9c0deedaec6
josephmanalo <josephmanalo21777@gmail.com>
2017-05-31 13:33:10 +0800

    Added Event Model

---
+++ QBO Events Management/Models/EventModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QBO_Events_Management.Models
+{
+	public class Event
+	{
+		public int eventID { get; set; }
+		public string Name { get; set; }
+		public string Details { get; set; }
+
+
+	}
+}
