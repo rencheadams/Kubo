e2d7173f227f2070194a0783a1a7c949539a0d38
Renche Adams C. Dalida <renche.dalida@spectres.solutions>
2017-06-08 12:52:34 +0800

    Added EventsController, and Events Add/Edit.

--- QBO Events Management/Models/EventModels.cs
+++ QBO Events Management/Models/EventModels.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace QBO_Events_Management.Models
 {
-	public class Event
-	{
-		public int eventID { get; set; }
-		public string Name { get; set; }
-		public string Details { get; set; }
-		public DateTime Date { get; set; }
-
-
+    public class Event
+    {
+        public int eventID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Venue { get; set; }
+    
+        [DataType(DataType.MultilineText)]
+        public string Details { get; set; }
 	}
+
+    public class EventDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+    }
 }
